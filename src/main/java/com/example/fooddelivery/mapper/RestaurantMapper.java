bad0fd31995f8a587254597470228105b8fc1d50
Egor_Lesckovsckey <zitreezyx@gmail.com>
2026-02-15 21:06:53 +0300

    add mapper

---
+++ src/main/java/com/example/fooddelivery/mapper/RestaurantMapper.java
@@ -0,0 +1,19 @@
+package com.example.fooddelivery.mapper;
+
+import com.example.fooddelivery.dto.RestaurantShortDto;
+import com.example.fooddelivery.entity.Restaurant;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class RestaurantMapper {
+    public RestaurantShortDto toShortDto(Restaurant restaurant){
+        if (restaurant == null)
+            return null;
+        RestaurantShortDto resShortDto = new RestaurantShortDto();
+        resShortDto.setId(restaurant.getId());
+        resShortDto.setName(restaurant.getName());
+        resShortDto.setRating(restaurant.getRating());
+        return resShortDto;
+    }
+}
