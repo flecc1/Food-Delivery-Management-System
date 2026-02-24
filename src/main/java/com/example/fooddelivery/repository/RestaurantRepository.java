35c6fd1f105f6867ba83a618746e94b230d6d9e1
Egor_Lesckovsckey <zitreezyx@gmail.com>
2026-02-25 00:02:16 +0300

    fix error in service

--- src/main/java/com/example/fooddelivery/repository/RestaurantRepository.java
+++ src/main/java/com/example/fooddelivery/repository/RestaurantRepository.java
@@ -4,5 +4,9 @@ import com.example.fooddelivery.entity.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {}
+public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+    List<Restaurant> findByName(String name);
+}
