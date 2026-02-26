936295493e67df007ecb5cd5a4531eb0d6f7689f
Egor_Lesckovsckey <zitreezyx@gmail.com>
2026-02-27 00:10:43 +0300

    finish entity customer and add CRUD operations

---
+++ src/main/java/com/example/fooddelivery/controller/CustomerController.java
@@ -0,0 +1,40 @@
+package com.example.fooddelivery.controller;
+
+import com.example.fooddelivery.dto.customer.CustomerCreateDto;
+import com.example.fooddelivery.dto.customer.CustomerDto;
+import com.example.fooddelivery.service.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/customers")
+@RequiredArgsConstructor
+public class CustomerController {
+    private final CustomerService customerService;
+    @GetMapping("/{id:\\d+}")
+    public CustomerDto getCustomerById(@PathVariable Long id){
+        return customerService.findCustomerById(id);
+    }
+
+    @GetMapping
+    public List<CustomerDto> getAllCustomers() {
+        return customerService.getCustomers();
+    }
+
+    @PostMapping
+    public CustomerDto addCustomer(@RequestBody CustomerCreateDto customerCreateDto){
+        return customerService.addCustomer(customerCreateDto);
+    }
+
+    @PutMapping("/{id:\\d+}")
+    public CustomerDto updateCustomer(@PathVariable Long id, @RequestBody CustomerCreateDto customerCreateDto){
+        return customerService.updateCustomer(id, customerCreateDto);
+    }
+
+    @DeleteMapping("/{id:\\d+}")
+    public void deleteCustomer(@PathVariable Long id){
+        customerService.deleteCustomerById(id);
+    }
+}
