ddd4cf1cedf54a37359a3c9f44fdfb4e8e261b25
Egor_Lesckovsckey <zitreezyx@gmail.com>
2026-03-22 17:43:56 +0300

    add log for category service

--- src/main/java/com/example/fooddelivery/service/CategoryService.java
+++ src/main/java/com/example/fooddelivery/service/CategoryService.java
@@ -24,40 +24,59 @@ public class CategoryService {
     private static final String CATEGORY_NOT_FOUND_MSG = "Category not found with id: ";
 
     public CategoryDto findCategoryById(Long id) {
-        return categoryRepository.findWithDishesById(id)
+        log.debug("try to find category with id: {}", id);
+        CategoryDto dto = categoryRepository.findWithDishesById(id)
                 .map(categoryMapper::toDto)
                 .orElseThrow(() -> new CategoryNotFoundException(CATEGORY_NOT_FOUND_MSG + id));
+        log.info("found category: with name: {}, id:  {})", dto.getName(), id);
+        return dto;
     }
 
     public Page<CategoryDto> getAllCategories(Pageable pageable) {
-        return categoryRepository.findAll(pageable).map(categoryMapper::toDto);
+        log.debug("try to find all categories");
+        Page<CategoryDto> categoryPage = categoryRepository.findAll(pageable)
+                .map(categoryMapper::toDto);
+        log.info("found all categories");
+        return categoryPage;
     }
 
     public Page<CategoryDto> findCategoryByName(String name, Pageable pageable) {
-        return categoryRepository.findCategoryByName(name, pageable).map(categoryMapper::toDto);
+        log.debug("try to find category with name: {}", name);
+        Page<CategoryDto> categoryPage = categoryRepository.findCategoryByName(name, pageable)
+                .map(categoryMapper::toDto);
+        log.info("found category: with name: {}", name);
+        return categoryPage;
     }
 
     @Transactional
     public CategoryDto addCategory(CategoryCreateDto categoryCreateDto) {
+        log.debug("try to add category: {}", categoryCreateDto.getName());
         Category category = categoryMapper.toEntity(categoryCreateDto);
-        return categoryMapper.toDto(categoryRepository.save(category));
+        Category save = categoryRepository.save(category);
+        log.info("save category: with name: {}, id:  {})", category.getName(), category.getId());
+        return categoryMapper.toDto(save);
     }
 
     @Transactional
     public void deleteCategoryById(Long id) {
+        log.debug("try to delete category with id: {}", id);
         Category category = categoryRepository.findById(id)
                 .orElseThrow(() -> new CategoryNotFoundException(CATEGORY_NOT_FOUND_MSG + id));
+        log.debug("checking dishes for category with id: {}", id);
         if (!category.getDishes().isEmpty()) {
             throw new CategoryHasDishesException("category with id: " + id + " has dishes cannot be deleted");
         }
+        log.info("delete category: with name: {}, id:  {})", category.getName(), id);
         categoryRepository.deleteById(id);
     }
 
     @Transactional
     public CategoryDto updateCategoryById(Long id, CategoryCreateDto categoryCreateDto) {
+        log.debug("try to update category with id: {}, name: {}", id, categoryCreateDto.getName());
         Category save = categoryRepository.findWithDishesById(id)
                 .orElseThrow(() -> new CategoryNotFoundException(CATEGORY_NOT_FOUND_MSG + id));
         save.setName(categoryCreateDto.getName());
+        log.info("update category: with name: {}, id:  {})", save.getName(), id);
         return categoryMapper.toDto(categoryRepository.save(save));
     }
 
