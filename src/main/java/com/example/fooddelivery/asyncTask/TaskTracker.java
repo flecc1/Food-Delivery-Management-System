7bd7a4e59df61c90fba122132790d573b78dc187
Egor_Lesckovsckey <zitreezyx@gmail.com>
2026-04-05 15:59:33 +0300

    add taskTracker

---
+++ src/main/java/com/example/fooddelivery/asyncTask/TaskTracker.java
@@ -0,0 +1,26 @@
+package com.example.fooddelivery.asyncTask;
+
+import com.example.fooddelivery.status.TaskStatus;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+public class TaskTracker {
+    private final ConcurrentHashMap<UUID, TaskStatus> tasks = new ConcurrentHashMap<>();
+
+    public UUID createTask() {
+        UUID taskId = UUID.randomUUID();
+        tasks.put(taskId, TaskStatus.PENDING);
+        return taskId;
+    }
+
+    public TaskStatus getTaskStatus(UUID taskId) {
+        return tasks.get(taskId);
+    }
+
+    public void updateTask(UUID taskId, TaskStatus newStatus) {
+        tasks.put(taskId, newStatus);
+    }
+}
